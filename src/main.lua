59da0f69aea3ae6980336b50cacfb51372282dab
leonardus <leonardus@leonardus.me>
2021-04-16 18:34:21 -0400

    Listen for clients

--- src/main.lua
+++ src/main.lua
@@ -1,5 +1,6 @@
 local config = require("nbtd.config")
 local cqueues = require("cqueues")
+local socket = require("cqueues.socket")
 local torrents = require("nbtd.torrents")
 
 math.randomseed(cqueues.monotime()*10000000)
@@ -10,3 +11,26 @@ torrents.init(
 		or os.getenv("XDG_DATA_HOME") .. "/nbtd"
 		or "/usr/share/nbtd"
 )
+
+local controller = cqueues.new()
+
+local function main()
+	local host, port = config.parsed.net.addr, config.parsed.net.port
+	local server = socket.listen(host, port)
+	print("[nbtd] Listening on " .. host .. ":" .. tostring(port))
+	while true do
+		local client = server:accept()
+		controller:wrap(function()
+			while true do
+				if socket.type(client) == "closed socket" then
+					break
+				end
+				local line = client:read("*l")
+				if line == nil then client:close() break end
+			end
+		end)
+	end
+end
+
+controller:wrap(main)
+assert(controller:loop())
